39ca911e6fa2ab7c672d32bd40efdfb99cbed1f2
Ziv Rush <ziv.r@taboola.com>
2020-05-22 18:11:54 +0300

    need to add maps. api

--- contentPostalHelper.js
+++ contentPostalHelper.js
@@ -25,6 +25,6 @@ function popHelper() {
 }
 
 if (dictionary().some((keyword) => wordInPage(keyword))) {
-    console.log("Found the word postal reference on page. popping PostalHelper");
+    console.log("Found the postal reference on page. popping PostalHelper");
     popHelper();
 }
